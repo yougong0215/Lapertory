0f08013a680ceff683162d44717cbcde25d869ea
VsPot07 <cocoacake84@gmail.com>
2023-03-18 21:51:28 +0900

    03/18
    의미없는 작업(실험) 완료

--- Assets/01_Script/MaeStro/Scripts/Skill/PlayerSkillSelecter.cs
+++ Assets/01_Script/MaeStro/Scripts/Skill/PlayerSkillSelecter.cs
@@ -18,6 +18,7 @@ public class PlayerSkillSelecter : MonoBehaviour
     private string _skillInfo;
 
     [Header("카드 요소")]
+    private GameObject _skillCard;
     TextMeshProUGUI nameText;
     TextMeshProUGUI coolTime;
     TextMeshProUGUI info;
@@ -25,9 +26,12 @@ public class PlayerSkillSelecter : MonoBehaviour
     {
         _playerSkill = GameObject.Find("Player").GetComponent<PlayerSkill>();
     }
-    private void Start()
+    private void Update()
     {
-        SkillSelectEvent(); // 임시
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            SkillSelectEvent(); // 임시
+        }
     }
     public void SkillSelectEvent()
     {
@@ -40,24 +44,27 @@ public class PlayerSkillSelecter : MonoBehaviour
     }
     private void SkillInfoSetting(int num)
     {
-        GameObject skillCard = Instantiate(_skillCardList[num], _skillCardMaster.transform);
+        _skillCard = Instantiate(_skillCardList[num], _skillCardMaster.transform);
 
-        skillCard.name = $"SkillCard_{num + 1}";
-        skillCard.transform.localPosition = _skillCardPos[num]; 
+        _skillCard.name = $"SkillCard_{num + 1}";
+        _skillCard.transform.localPosition = _skillCardPos[num]; 
 
         #region  텍스트 받기
-        nameText = skillCard.transform.Find("SkillName").GetComponent<TextMeshProUGUI>();
-        coolTime = skillCard.transform.Find("SkillCoolTime").GetComponent<TextMeshProUGUI>();
-        info = skillCard.transform.Find("SkillInfo").GetComponent<TextMeshProUGUI>();
+        nameText = _skillCard.transform.Find("SkillName").GetComponent<TextMeshProUGUI>();
+        coolTime = _skillCard.transform.Find("SkillCoolTime").GetComponent<TextMeshProUGUI>();
+        info = _skillCard.transform.Find("SkillInfo").GetComponent<TextMeshProUGUI>();
         #endregion
         nameText.text = _skillCardData.skillData. skillName;
         coolTime.text = _skillCardData.skillData.skillCoolTime.ToString();
         info.text = _skillCardData.skillData.skillInfo;
-    }
 
+        
+    }
     private void SkillInfoDetection()
     {
         int random = random = Random.Range(0, _skillCardList.Count);
         _skillCardData = _skillCardList[random].GetComponent<SkillCard>();
     }
+
+    
 }
