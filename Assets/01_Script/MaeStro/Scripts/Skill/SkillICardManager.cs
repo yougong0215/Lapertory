d7c257ad579cd3df3ed81a55a8993be15fbd85ac
VsPot07 <cocoacake84@gmail.com>
2023-03-20 00:32:07 +0900

    ㄴㄱㅎㅋ
    ㅎㅠ

--- Assets/01_Script/MaeStro/Scripts/Skill/SkillICardManager.cs
+++ Assets/01_Script/MaeStro/Scripts/Skill/SkillICardManager.cs
@@ -28,6 +28,7 @@ public class SkillICardManager : MonoBehaviour
     }
     IEnumerator SelectCardEventCoroutine(GameObject selectCard)
     {
+        //dd
         selectCard.transform.DOScale(new Vector3(1.2f, 1.2f), 0.5f);
         yield return new WaitForSeconds(0.5f);
         selectCard.transform.DOLocalMoveY(-900, 0.7f).SetEase(Ease.InBack);
