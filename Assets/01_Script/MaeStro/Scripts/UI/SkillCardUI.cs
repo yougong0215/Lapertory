6eb5a5406017aadb17cbbe2be2bb8bbd8c3c76da
VsPot07 <cocoacake84@gmail.com>
2023-03-25 19:58:16 +0900

    03/25
    키 입력 저장 완료

--- Assets/01_Script/MaeStro/Scripts/UI/SkillCardUI.cs
+++ Assets/01_Script/MaeStro/Scripts/UI/SkillCardUI.cs
@@ -8,7 +8,12 @@ public class SkillCardUI : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI keyText;
 
+    SkillBase skillBase;
+    bool isCangeKey;
+    KeyCode changeKey;
 
+    Color selectColor = new Color(1, 0.9f, 0);
+    Color normalColor = new Color(0, 0, 0);
     private void Awake()
     {
         nameText = transform.Find("NameText").GetComponent<TextMeshProUGUI>();
@@ -17,9 +22,33 @@ public class SkillCardUI : MonoBehaviour
 
     public void ValueSetting(GameObject _skilLData)
     {
-        SkillBase skillBase = _skilLData.GetComponent<SkillBase>();
+        skillBase = _skilLData.GetComponent<SkillBase>();
 
         nameText.text = skillBase.skillName;
         keyText.text = skillBase.skillKey.ToString();
+    }
+
+    private void OnGUI()
+    {
+        if(isCangeKey)
+        {
+            Event keyChangeEvent = Event.current;
+            if (Input.anyKeyDown)
+            {
+                changeKey = keyChangeEvent.keyCode;
+                skillBase.skillKey = changeKey;
+                keyText.text = changeKey.ToString();
+                keyText.color = normalColor;
+                isCangeKey = false;
+            }
+        }
+        
+    }
+
+    public void ChangeKey()
+    {
+        isCangeKey = !isCangeKey;
+        keyText.color = selectColor;
+        keyText.text = $"> {skillBase.skillKey} <";
     }
 }
