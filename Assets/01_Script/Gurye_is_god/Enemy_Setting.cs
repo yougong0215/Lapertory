934e39189ed4d62261a2a1f89ed68849681a9c55
KoDaMin <ko060614@gmail.com>
2023-03-27 00:46:03 +0900

    ..

--- Assets/01_Script/Gurye_is_god/Enemy_Setting.cs
+++ Assets/01_Script/Gurye_is_god/Enemy_Setting.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemySetting", menuName = "SO")]
+[SerializeField]
 public class Enemy_Setting : ScriptableObject
 {
-    float Damage { get; set; }
-    float Stoppingdistance { get; set; }
-
-    enum job
-    {
-        melee,
-        LongDistanceAttack,
-    }
+    [SerializeField] private float maxhp;
+    public float MaxHp => maxhp;
+    [SerializeField] private float damage;
+    public float Damage => damage;
+    [SerializeField] private bool isLongDistanceAttack;
+    public bool IsLongDistanceAttack => isLongDistanceAttack;
+    [SerializeField] private float attackRange;
+    public float AttackRange => attackRange;
+    [SerializeField] private float timeBetweenAttack;
+    public float TimeBetweenAttacks => timeBetweenAttack;
 }
