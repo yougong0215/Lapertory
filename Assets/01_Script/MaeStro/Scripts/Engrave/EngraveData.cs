e120aa9bf5626fd220cf2c69fba155dacf787268
VsPot07 <cocoacake84@gmail.com>
2023-04-01 18:19:16 +0900

    04/01 에셋 받음

--- Assets/01_Script/MaeStro/Scripts/Engrave/EngraveData.cs
+++ Assets/01_Script/MaeStro/Scripts/Engrave/EngraveData.cs
@@ -8,6 +8,5 @@ public class EngraveData : ScriptableObject
     [SerializeField] private string _engraveName;
     public string engraveName { get { return _engraveName; } }
 
-    [SerializeField] private GameObject _engraveObject;
-    public GameObject engraveObject { get { return _engraveObject; } }
+    
 }
